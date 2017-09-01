8f6ab1db07e924933928aa8d8d77290930274b20
neutralioJenkins <jenkins@n.io>
2017-09-01 12:21:21 -0600

    Bump version to v0.1.0

--- mqtt_publish_block.py
+++ mqtt_publish_block.py
@@ -6,7 +6,7 @@ from .mqtt_base_block import MqttBase
 
 class MqttPublish(MqttBase, TerminatorBlock):
 
-    version = VersionProperty('0.1.0')
+    version = VersionProperty("0.1.0")
 
     def process_signals(self, signals):
         for signal in signals:
